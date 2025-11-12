62c1aa66efb5b78b9dfeddbdfd698b8de6c74625
Msolis314 <solisfanny2021@gmail.com>
2025-11-12 23:46:15 +0000

    Branch de roi

---
+++ src/roi.cpp
@@ -0,0 +1,3 @@
+#include <gst/gst.h>
+#include <glib.h>
+#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <#include <
