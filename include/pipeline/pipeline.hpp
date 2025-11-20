b6c44e51b59dbd09adff0982c313296cc73152d4
Daniela Fonseca <daniela.fonsecazumbado@gmail.com>
2025-11-19 19:42:31 -0600

    Declarar métodos públicos y privados

--- include/pipeline/pipeline.hpp
+++ include/pipeline/pipeline.hpp
@@ -1,8 +1,8 @@
-#ifdef PIPELINE_HPP
+#ifndef PIPELINE_HPP
 #define PIPELINE_HPP
 
-#include <unordered_map>
 #include <gst/gst.h>
+#include <unordered_map>
 #include "config/app_config.hpp"
 #include "config/track_info.hpp"
 #include "roi/roi.hpp"
@@ -15,13 +15,24 @@ public:
     // Destructor
     ~Pipeline();
 
+    // Public methods
+    bool init();
+    void run();
+
 private:
+    // Pipeline state
     GstElement *pipeline;
     GMainLoop *loop;
+
+    // Logical state
     AppConfig config;
     ROIParams roi;
     std::unordered_map<guint64, TrackInfo> tracked_objects;
+    
+    // Timing
     GTimer *app_timer;
+
+    // Metrics
     guint total_detected;
     guint total_alerts;
     gint source_width;
@@ -29,9 +40,17 @@ private:
     gboolean roi_has_objects;
     gboolean roi_has_alerts;
 
-    // Methods
-    static gboolean create_pipeline();
-    static void cleanup();
+private:
+    // Private methods
+    gboolean create_pipeline();
+    void cleanup();
+    void handle_bus_message(GstMessage *msg);
+    GstPadProbeReturn handle_pad_probe(GstPadProbeInfo *info);
+    
+    // Static callbacks (for Gstreamer)
+    static GstPadProbeReturn pad_probe_callback(GstPad *pad, GstPadProbeInfo *info, gpointer udata);
+    static gboolean bus_callback(GstBus *bus, GstMessage *msg, gpointer data);
+    static void on_pad_added_callback(GstElement *element, GstPad *pad, gpointer data);
 };
 
 #endif // PIPELINE_HPP
